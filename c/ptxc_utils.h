d6944f153ccafaa260a088e2a4a10bbf8b6b9779
sree <sree@buckyball>
2020-04-29 21:19:39 -0400

    Add SAT, new version of xir, but ptxc.c is broken due to FTZ and literal_type

--- c/ptxc_utils.h
+++ c/ptxc_utils.h
@@ -5,6 +5,11 @@
 #define FTZ(X) _Generic((X), \
 						float: FTZf,			\
 						double: FTZd)(X)
+
+#define SAT(X) _Generic((X), \
+						float: SATf,			\
+						double: SATd)(X)
+
 #endif
 
 static inline float FTZf(float x) {
@@ -22,3 +27,27 @@ static inline float FTZd(double x) {
 
   return x;
 }
+
+static inline float SATf(float x) {
+  if(isnan(x)) return 0.0;
+
+  if(x < 0.0)
+	return 0.0;
+  else if (x > 1.0)
+	return 1.0;
+  else
+	return x;
+}
+
+static inline double SATd(double x) {
+  if(isnan(x)) return 0.0;
+
+  if(x < 0.0)
+	return 0.0;
+  else if (x > 1.0)
+	return 1.0;
+  else
+	return x;
+}
+
+/* note: add_sat_s32 is not a separate operator because of overflow */
