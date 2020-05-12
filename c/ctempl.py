3bbba444681f2d9654efee5f2ea07b3548f0e699
sree <sree@buckyball>
2020-05-12 14:28:12 -0400

    Add ctempl.py, to ultimately generate ptxc_utils.h

---
+++ c/ctempl.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+
+import argparse
+import pycparser
+from pycparser import c_ast, c_generator, c_parser
+import tempfile
+from collections import OrderedDict
+import copy
+
+class Instantiate(c_ast.NodeVisitor):
+    def visit_IdentifierType(self, node):
+        if len(node.names) == 1:
+            v = node.names[0]
+            if v in self._dtvast:
+                #print(v, self._dtvast[v])
+                node.names = self._dtvast[v].type.names
+
+    def visit_FuncDef(self, node):
+        # there is only one Funcdef
+
+        # this doesn't change the name ...
+        node.decl.name = self._funcname
+
+        # funcdecl.decl).type.declname
+        node.decl.type.type.declname = self._funcname
+        self.generic_visit(node)
+
+    def instantiate(self, dtvast, tmplast, funcname):
+        self._dtvast = dtvast
+        self._funcname = funcname
+        self.visit(tmplast)
+
+def load_template(cfile, headers):
+    src = pycparser.parse_file(cfile, use_cpp=True, cpp_args=["-I{headers}"])
+    out = {}
+    template_vars = {}
+    for d in src.ext: # should work in pycparse < 2.19
+        if isinstance(d, c_ast.Typedef):
+            if d.coord.file == cfile:
+                if d.name[0] == "T": # unnecessary convention?
+                    template_vars[d.name] = d
+        elif isinstance(d, c_ast.FuncDef):
+            n = d.decl.name
+            out[n] = d
+
+    return template_vars, out
+
+def get_typedefs_for_tvars(varvalues):
+    o = []
+    for v in varvalues:
+        v, vt = v.split("=")
+        o.append((v, f"typedef {vt} {v};"))
+
+    return OrderedDict(o)
+
+def instantiate(tmplname, funcname, tvals, ctempl, tvars, headers):
+    assert tmplname in ctempl, f"Unknown template function {tmplname}"
+
+    # parse template variable instantiations
+    with tempfile.NamedTemporaryFile(mode='w', suffix=".c") as f:
+        f.write(headers)
+        ccode = "\n".join([v for v in tvals.values()])
+        f.write(ccode)
+        f.flush()
+
+        tint = pycparser.parse_file(f.name, use_cpp=True, cpp_args=["-I{headers}"])
+
+    # extract ASTs
+    out = {}
+    for d in tint.ext:
+        if isinstance(d, c_ast.Typedef):
+            if d.name in tvars:
+                out[d.name] = d.type
+
+    tmpl = copy.deepcopy(ctempl[tmplname])
+
+    ti = Instantiate()
+    ti.instantiate(out, tmpl, funcname)
+
+    cg = c_generator.CGenerator()
+    return out, cg.visit(tmpl)
+
+def generate_generics(generics, instantiations):
+    for g in generics:
+        tmplname, args, tvar_typename, control = g
+        args = ', '.join(args.split(":"))
+        assert tmplname in instantiations, f"Generic {tmplname} has not been instantiated"
+
+        assoc_list = []
+        for i in instantiations[tmplname]:
+            funcname, tvals = i
+            # typedef XYZ ABC T0;
+            tval_typename = tvals[tvar_typename][len("typedef "):-len(tvar_typename)-1] # yuck...
+            assoc_list.append((tval_typename.strip(), funcname))
+
+        assoc_list = ", ".join([f"({t}): {v}" for (t, v) in assoc_list])
+        print(f"#define {tmplname}({args}) _Generic(({control}, {assoc_list})({args})")
+
+def process_script(script, fakecheaders = '/usr/share/python-pycparser/fake_libc_include'):
+    ctempl = None
+    headers = []
+    instantiations = {}
+    generics = []
+
+    with open(script, "r") as f:
+        for l in f:
+            l = l.strip()
+            if not l: continue # blank line
+            if l[0] == "#": continue # comment
+
+            if l.startswith("template "):
+                assert ctempl is None, f"Duplicate template lines in script"
+                ctempl = ' '.join(l.split()[1:])
+            elif l.startswith("tvar_include "):
+                hdr = l.split(' ', 1)[1]
+                headers.append(f"#include {hdr.strip()}")
+            elif l.startswith("inst"):
+                # inst tmplfunc funcname tvar1=tval1:tvar2=tval2
+                inst = l.split()
+                tvars = get_typedefs_for_tvars(inst[3].split(':'))
+                if inst[1] not in instantiations:
+                    instantiations[inst[1]] = []
+
+                instantiations[inst[1]].append((inst[2], tvars))
+            elif l.startswith("generic"):
+                gen = l.split()
+                if len(gen) > 5:
+                    gen[4] = ' '.join(gen[4:]) # expression is last and can contain ' '
+
+                generics.append((gen[1], gen[2], gen[3], gen[4]))
+
+    tvars, ctempl = load_template(ctempl, fakecheaders)
+    headers = "\n".join(headers) + "\n"
+    print("#pragma once")
+    print(headers)
+    for tmplfunc in instantiations.keys():
+        for funcname, tvals in instantiations[tmplfunc]:
+            _, code = instantiate(tmplfunc, funcname, tvals, ctempl, tvars, headers)
+            print(code)
+
+    generate_generics(generics, instantiations)
+
+if __name__ == "__main__":
+    import sys
+
+    if sys.argv[0].endswith("ctempl_script.py"):
+        p = argparse.ArgumentParser(description="Produce `generic' C code from templates")
+
+        p.add_argument("script", help="C template file using typedefs as templates")
+        p.add_argument('--fakecheaders', help="Fake C headers for pycparser", default="/usr/share/python-pycparser/fake_libc_include") # this assumes that a pycparser package is installed
+
+        args = p.parse_args()
+        process_script(args.script)
+    else:
+        p = argparse.ArgumentParser(description="Produce `generic' C code from templates")
+
+        p.add_argument("ctempl", help="C template file using typedefs as templates")
+        p.add_argument("tmplfunc", help="Function template name")
+        p.add_argument("funcname", help="Function instantiatiation name")
+        p.add_argument("variables", nargs="+", help="Template variable assignments (templatevar=templatetype)")
+        p.add_argument('--fakecheaders', help="Fake C headers for pycparser", default="/usr/share/python-pycparser/fake_libc_include") # this assumes that a pycparser package is installed
+
+        args = p.parse_args()
+
+        tvars, ctempl = load_template(args.ctempl, args.fakecheaders)
+        tvals = get_typedefs_for_tvars(args.variables)
+        instantiate(args.tmplfunc, args.funcname, tvals, ctempl, tvars, "#include <stdint.h>\n")
+
---
+++ c/ctempl_script.py
@@ -0,0 +1,1 @@
+ctempl.py
